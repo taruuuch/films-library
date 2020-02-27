8f4f70beeb2c56dfe81c7515ba586617e95ef101
Dmytro Artemchuk <taruuuch@gmail.com>
2020-02-27 22:16:42 +0200

    Add logger to server, update README

--- server/app.js
+++ server/app.js
@@ -2,32 +2,33 @@ const express = require('express')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const morgan = require('morgan')
 const setupRoutes = require('./utils/routes.util')
-const { PORT } = require('./config/base.config')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(morgan('[:method] :url :status :res[content-length] - :response-time ms'))
 // Security setting
 app.use(cors({
     origin: '*',
     methods: 'GET, POST, PATCH, DELETE'
 }))
 app.use(helmet())
-// app.use(rateLimit({
-//     windowMs: 15 * 60 * 1000,
-//     max: 100,
-//     message: 'Too many accounts created from this IP, please try again after an hour'
-// }))
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many accounts created from this IP, please try again after an hour'
+}))
 
 // Settuping RESP API routes
 setupRoutes(app)
 
 // Handling errors
 app.use(async (req, res, next) => {
-    const error = new Error(`Api link not found! Go to localhost:${PORT} for check available links`)
+    const error = new Error(`API link not found!`)
     error.status = 404
     error.path = `${req.protocol}://${req.get('host')}${req.originalUrl}`
 
